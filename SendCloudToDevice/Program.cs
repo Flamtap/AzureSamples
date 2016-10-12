1f530dafd2dc4c5b812ac2207f09955c282746da
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-11 22:07:50 -0600

    Message sending methods
    Methods for sending C2D messages

--- SendCloudToDevice/Program.cs
+++ SendCloudToDevice/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
 
 namespace SendCloudToDevice
 {
     public class Program
     {
+        public static ServiceClient ServiceClient;
+        public static string ConnectionString = "";
+
         public static void Main(string[] args)
         {
+            Console.WriteLine("Send Cloud-to-Device message...\n");
+            ServiceClient = ServiceClient.CreateFromConnectionString(ConnectionString);
+
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+
+            SendCloudToDeviceMessageAsync().Wait();
+
+            Console.ReadLine();
+        }
+
+        private static async Task SendCloudToDeviceMessageAsync()
+        {
+            Message commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+
+            await ServiceClient.SendAsync("esDevice", commandMessage);
         }
     }
 }
